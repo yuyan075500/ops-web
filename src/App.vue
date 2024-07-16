81e0d38e751d95f269db789001a50022420eb269
yuyan <yuyan@50yc.com>
2024-07-16 15:42:55 +0800

    update：设置diglog全局居中

--- src/App.vue
+++ src/App.vue
@@ -9,3 +9,13 @@ export default {
   name: 'App'
 }
 </script>
+
+<style>
+/* 全局配置dialog上下左右居中 */
+.el-dialog {
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  margin: 0px !important;
+}
+</style>

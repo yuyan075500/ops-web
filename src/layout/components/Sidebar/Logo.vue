e25f00cc26f71d0d507727f870f8fad40f5ca77c
yuyan <yuyan@50yc.com>
2024-12-06 16:12:36 +0800

    update：Logo更换

--- src/layout/components/Sidebar/Logo.vue
+++ src/layout/components/Sidebar/Logo.vue
@@ -25,7 +25,7 @@ export default {
   data() {
     return {
       title: process.env.VUE_APP_TITLE,
-      logo: 'https://wpimg.wallstcn.com/69a1c46c-eb1c-4b46-8bd4-e9e686ef5251.png'
+      logo: require('@/assets/logo.png')
     }
   }
 }
